e391038b98303bead48a7c34ab2147c9e979ab2b
Arvid Edenheim <arved490@student.liu.se>
2019-04-16 14:06:37 +0200

    Task 1 & 2 done

--- Lab1/lab1_1_b2.py
+++ Lab1/lab1_1_b2.py
@@ -1,31 +1,41 @@
 import time
 import csv
 
-def init():
+def init(years):
     with open('/nfshome/hadoop_examples/shared_data/temperatures-big.csv') as data:
+    # with open('./test.txt') as data:
         reader = csv.reader(data)
         for row in reader:
-            print(row)
-
-    return []
-
-
-def reduce_by_key(data):
-    return []
+            lines = row[0].split(";")
+            years = filter_by_year(lines, years)
+    return years
 
 
-def filter_by_year(data, start_year=1950, end_year=2014):
-    return []
+def filter_by_year(row, years, start_year=1950, end_year=2014):
+    year = int(row[1][0:4])
+    temp = float(row[3])
+    if year >= 1950 and year <= 2014:
+        if not year in years:
+            years[year] = temp
+        elif years[year] < temp:
+            years[year] = temp
+    return years
 
+
+def write_to_file(data):
+    f = open("results.txt", "w")
+    for row in data:
+        f.write(str(row) + ' : ' + str(data[row]) + '\n')
+    f.close()
 
 def main():
     start_time = time.time()
-
-    data = init()
-    filtered_data = filter_by_year(data)
+    years = {}
+    years = init(years)
     elapsed_time = time.time() - start_time
-
-    print(elapsed_time)
+    write_to_file(years)
+    print('Elapsed time:', elapsed_time)
+    print('Max temperatures:', years)
 
 
 main()
--- Lab1/lab1_2.py
+++ Lab1/lab1_2.py
@@ -1,22 +1,18 @@
 from pyspark import SparkContext
 
 # Misc input and setup
-sc = SparkContext(appName = "assignment 1")
+sc = SparkContext(appName = "lab_1_2_a")
 temperature_file = sc.textFile("/user/x_arved/data/temperature-readings.csv")
 lines = temperature_file.map(lambda line: line.split(";"))
-year_temperature = lines.map(lambda x: (x[1][0:4], x[1][5:7], float(x[3])))
+year_temperature = lines.map(lambda x: ((x[1][0:4], x[1][5:7]), float(x[3])))
 
-# Filter all readings before 1950 and after 2014
-year_temperature = year_temperature.filter(lambda x: int(x[0])>=1950 and int(x[0])<=2014)
+# Filter all readings before 1950 and after 2014 which has a temperature above 10 degrees
+year_temperature = year_temperature.filter(lambda x: int(x[0][0])>=1950 and int(x[0][0])<=2014 and x[1] > 10)
 
-# MapReduce, reduce by temperature
-max_temperatures = year_temperature.reduceByKey(lambda a,b: a if a>=b else b)
-min_temperatures = year_temperature.reduceByKey(lambda a,b: a if a<b else b)
-
-# Sort by temperature, index = 2
-max_temperatures_sorted = max_temperatures.sortBy(ascending=False, keyfunc = lambd k: k[2])
-min_temperatures_sorted = min_temperatures.sortBy(ascending=False, keyfunc = lambd k: k[2])
+occurences = year_temperature.countByKey()
 
 # Save to file
-max_temperatures_sorted.saveAsTextFile("max_temperature")
-min_temperatures_sorted.saveAsTextFile("min_temperature")
+f = open('./results/results_1_2.txt', 'w')
+for keys, count in occurences.items():
+    f.write(str(keys[0]) + ',' + str(keys[1]) + ',' + str(count) + '\n')
+f.close()

299567b38c8b0d0a78bc82dbf46069f0cb126ae3
jonbj587 <jonbj587@student.liu.se>
2019-05-07 13:42:18 +0200

    started lab 2 part 2

--- Lab2/lab2_2_api.py
+++ Lab2/lab2_2_api.py
@@ -1,4 +1,4 @@
-# Big Data Analytics - BDA2 - 1
+# Big Data Analytics - BDA2 - 2 api
 from pyspark import SparkContext
 from pyspark.sql import SQLContext, Row
 from pyspark.sql import functions as F
@@ -15,21 +15,21 @@ schemaTempReadings = sqlContext.createDataFrame(tempReadings)
 schemaTempReadings.registerTempTable("tempReadings")
 
 # Lowest and highest temperature measured each year.
-schemaTempReadingsMax = schemaTempReadings.groupBy('year', 'station') \
+schemaTempReadingsDistinct = schemaTempReadings.groupBy('year', 'station') \
                                           .agg(F.min('value') \
                                           .alias('dailymax')) \
                                           .orderBy(['year', 'station', 'dailymax'], ascending=[0,0,1])
 
-schemaTempReadingsMin = schemaTempReadings.groupBy('year', 'station') \
+schemaTempReadingsNotDistinct = schemaTempReadings.groupBy('year', 'station') \
                                           .agg(F.max('value') \
                                           .alias('dailymin')) \
                                           .orderBy(['year', 'station', 'dailymin'], ascending=[0,0,1])
 
+# Not distinct
+largerThan10Degrees = sqlContext.sql("SELECT year, month, count(value) as value FROM tempReadingsTable WHERE year=1950 and value>=10.0 group by year")
 
-#max = sqlContext.sql("SELECT year, station, max(value) as value FROM tempReadings WHERE year>=1950 AND year<=2014 ORDER BY value DESC")
-#min = sqlContext.sql("SELECT year, station, min(value) as value FROM tempReadings WHERE year>=1950 AND year<=2014 ORDER BY value DESC")
-
-# largerThan10Degrees = sqlContext.sql("SELECT year, month, count(value) as value FROM tempReadingsTable WHERE year=1950 and value>=10.0 group by year, month")
+# Distinct
+largerThan10Degrees = sqlContext.sql("SELECT year, month, count(value) as value FROM tempReadingsTable WHERE year=1950 and value>=10.0 group by year")
 
 # year, station with the max, maxValue ORDER BY maxValue DESC
 # year, station with the min, minValue ORDER BY minValue DESC
---
+++ Lab2/lab2_2_sql.py
@@ -0,0 +1,31 @@
+# Big Data Analytics - BDA2 - 2 sql
+from pyspark import SparkContext
+from pyspark.sql import SQLContext, Row
+from pyspark.sql import functions as F
+
+sc = SparkContext()
+sqlContext = SQLContext(sc)
+
+# Load a text file and convert each line to a Row.
+rdd = sc.textFile("../data/temperature-readings.csv")
+parts = rdd.map(lambda l: l.split(";"))
+tempReadings = parts.map(lambda p: Row(station=p[0], year=p[1].split("-")[0], value=float(p[3])))
+
+schemaTempReadings = sqlContext.createDataFrame(tempReadings)
+schemaTempReadings.registerTempTable("tempReadings")
+
+# Lowest and highest temperature measured each year.
+max = sqlContext.sql("SELECT year, station, max(value) as value FROM tempReadings WHERE year>=1950 AND year<=2014 ORDER BY value DESC")
+min = sqlContext.sql("SELECT year, station, min(value) as value FROM tempReadings WHERE year>=1950 AND year<=2014 ORDER BY value DESC")
+
+# Not distinct
+largerThan10Degrees = sqlContext.sql("SELECT year, month, count(value) as value FROM tempReadingsTable WHERE year=1950 and value>=10.0 group by year")
+
+# Distinct
+largerThan10Degrees = sqlContext.sql("SELECT year, month, count(value) as value FROM tempReadingsTable WHERE year=1950 and value>=10.0 group by year")
+
+# year, station with the max, maxValue ORDER BY maxValue DESC
+# year, station with the min, minValue ORDER BY minValue DESC
+
+schemaTempReadingsMax.write.mode('append').csv("./results/lab2_1_max")
+schemaTempReadingsMin.write.mode('append').csv("./results/lab2_1_min")

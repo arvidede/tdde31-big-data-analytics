15503a855818573c9b6d325149e69bf6e627a00c
jonbj587 <jonbj587@student.liu.se>
2019-05-14 12:06:15 +0200

    Fixed the code

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -7,26 +7,21 @@ from pyspark import SparkContext
 #sc = SparkContext(appName="lab_kernel")
 sc = SparkContext()
 sqlContext = SQLContext(sc)
+dateFormat = '%y-%m-%d'
 
-# Stations
+# Stations - station, lat, long
 stationsFile = sc.textFile("../data/stations.csv")
 stationsLines= stationsFile.map(lambda line: line.split(";"))
-stations = stationsLines.map(lambda p: Row(station=p[0], lat=float(p[3]), long=float(p[4]))
-schemaStations = sqlContext.createDataFrame(stations)
-schemaStations.createOrReplaceTempView("stations")
+stations = stationsLines.map(lambda x: ((x[0]), (float(x[3]), float(x[4]))))
+stations = dict(stations.collect())
 
-# Temperatures
+# Temperatures - station, date, time, temp, lat, long
 temperatureFile = sc.textFile("../data/temperature-readings.csv")
 temperatureLines = temperatureFile.map(lambda line: line.split(";"))
-tempReadings = temperatureLines.map(lambda p: Row(station=p[0], day=int(p[1].split("-")[2]), month=int(p[1].split("-")[1]), \
-                                                  year=int(p[1].split("-")[0]), value=float(p[3])))
-schemaTempReadings = sqlContext.createDataFrame(tempReadings)
-schemaTempReadings.createOrReplaceTempView("tempReadings")
+tempReadings = temperatureLines.map(lambda x: ((x[0], datetime.strptime(x[1], dateFormat), int(x[2][0:2]), float(x[3]), stations[x[0]][0], stations[x[0]][1]))
 
-# Join
-readings = schemaTempReadings.join(schemaStations, 'station')
 # Exclude all dates after 2013-07-04
-readings = readings.filter("year <= 2013 AND month <= 07 AND day <= 04")
+readings = readings.filter(lambda x: (int(x[1].year)<=2013 and int(x[1].month)<=7 and int(x[1].day)<=4))
 
 def haversine(lon1, lat1, lon2, lat2):
     # Calculate the great circle distance between two points on the earth (specified in decimal degrees)
@@ -41,42 +36,33 @@ def haversine(lon1, lat1, lon2, lat2):
     return km
 
 # Up to you
-h_distance = 250000 # Distance to station is important
-h_date = 25
-h_time = 5 
+h_distance = 100000
+h_date = 10
+h_time = 2
 a = 58.4274
 b = 14.826
-date = "2013-07-04"
-h = ("04:00:00", "06:00:00", "08:00:00", "10:00:00", "12:00:00", "14:00:00",\
-     "16:00:00", "18:00:00", "20:00:00", "22:00:00", "00:00:00")
+date = datetime.strptime("2013-07-04", dateFormat)
+h = (4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 0)
 
 # Your code here
 # Gaussian kernel
 def kernel(u):
     return(exp(-(abs(u)**2)))
 
-# dateDiff <- unclass((as.POSIXct(date) - as.POSIXct(st[i,]$date)))
-# dateDiff <- dateDiff %% 365
-# if (dateDiff > 365/2) {
-#    dateDiff <- 365 - dateDiff
-# }
+# Date
+def dateDiff(x):
+    return(abs(x.days % 365))
 
-# Time
-# if (timeDiff > 12) {
-#   timeDiff <- timeDiff - 12
-# }
 
 gaussianKernel = []
 for hour in h:
-    # Station, distance, date, time, temperature
-    kernels = schemaTempReadings.map(lambda x: (x[0], \
-                                                kernel(haversine(x[1][1], x[1][0], b, a) / h_distance), \
-                                                kernel(skillnad i dagar / h_date), \
-                                                kernel(skillnad i timmar / h_time), \
-                                                x[4]))
+    # station, date, time, temp, lat, long
+    kernels = readings.map(lambda x: (kernel(haversine(x[5], x[4], b, a) / h_distance), \
+                                      kernel(dateDiff(date - x[1]) / h_date), \
+                                      kernel((abs(hour - x[2]) % 12) / h_time), \
+                                      x[3]))
 
-    kernelSumDenominator = kernels.map(lambda x: (x[1] + x[2] + x[3]))
-    kernelSum = kernels.map(lambda x: (((x[1] + x[2] + x[3])*x[4])/kernelSumDenominator))
+    kernelSum = kernels.map(lambda x: (((x[0] + x[1] + x[2])*x[3]) / (x[0] + x[1] + x[2])))
     gaussianKernel.append(kernelSum)
 
 print gaussianKernel
